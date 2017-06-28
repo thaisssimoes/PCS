4fe5c4fbb6b0dbba7fb06004c8fa5683222b03e4
Thaïs Simões <thay.ssim@gmail.com>
2017-06-28 15:36:38 -0300

    Retiradas as linhas vazias da tabela.

--- Sistema/src/view/TelaAluno.java
+++ Sistema/src/view/TelaAluno.java
@@ -179,10 +179,6 @@ public class TelaAluno extends javax.swing.JFrame {
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null}
             },
             new String [] {
                 "Protocolo", "Tipo", "Status"
